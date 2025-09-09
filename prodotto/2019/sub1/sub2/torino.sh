d3b93dc00acea57c87c4089eb17ad7d61edafbcc
Manila <78729862+manilaexe@users.noreply.github.com>
2025-09-09 10:56:48 +0200

    Create torino.sh

---
+++ prodotto/2019/sub1/sub2/torino.sh
@@ -0,0 +1,5 @@
+Pane,PanettiereNord,Italia,90,1kg,ScaffaleA,20240103
+Latte,Parmalat,Italia,220,1L,Frigo1,20240107
+Mela,Golden,Spagna,310,500g,ScaffaleB,20240112
+Uova,FattoriaVerdi,Italia,170,6pz,Frigo2,20240105
+Pasta,Rummo,Italia,450,500g,ScaffaleC,20240106
